4e6e0ddf05c00c8f3f2c62ce3c627858e7eac51d
kong-tae-hyeon-khu <kong3047@khu.ac.kr>
2023-03-23 11:42:29 +0900

    Blog Project Start (with Django)

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/gongtaehyeon/2023-1/클라우드서비스/과제/HW3/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
